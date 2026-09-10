20dc654ab1a18cbf4e9c942531b162a5f009f8de
luc.perroquin <luc.perroquin@gmail.com>
2026-09-10 17:55:14 +0200

    Remove temporary CSI save script

--- .github/scripts/csi_save_current.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-source = Path('index.html')
-target = Path('index_save.html')
-
-data = source.read_bytes()
-target.write_bytes(data)
-print(f'Saved {len(data)} bytes from {source} to {target}')
